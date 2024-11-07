b6186896d57793fe93c0f562eb73253a42aee451
Emranul Hoque <122172406+emranrakib62@users.noreply.github.com>
2024-11-06 22:11:24 -0800

    Add files via upload

---
+++ WebtoAppConvert/app/src/main/java/com/example/webtoappconvert/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.webtoappconvert;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+WebView web;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        web=findViewById(R.id.web);
+        web.getSettings().setJavaScriptEnabled(true);
+        web.loadUrl("https://cityuniversity.orbund.com/einstein-freshair/index.jsp");
+    }
+}
